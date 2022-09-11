13d4cb5dc0aa53c798c23a2efbaf03ea4888bfec
AbhishekWaghchaure <abhisw28@gmail.com>
2022-09-11 11:05:14 +0530

    added functions

---
+++ Functions.py
@@ -0,0 +1,20 @@
+#inbuilt
+int()
+str()
+bool()
+float()
+dir()
+
+
+#module
+#math
+# import math
+from math import *
+print (sqrt(16))
+
+# custom function
+
+def printSum(first,second):
+    print(first + second)
+
+printSum(1, 2)
