0bfc88333e77a4b4046b4156874ec5727df8ecfd
AbhishekWaghchaure <abhisw28@gmail.com>
2022-09-11 08:42:19 +0530

    Added for and while loops

---
+++ ForLoops.py
@@ -0,0 +1,6 @@
+# range(0,5)
+for i in range (5):
+    print(i)
+
+for i in range (5):
+    print(i * "*")
--- WhileLoops.py
+++ WhileLoops.py
@@ -6,7 +6,7 @@ while i <= 5:
     print(i * "*")
     i = i+1
 
-i = 5
+i = 4
 while i >= 0:
     print(i * "*")
     i = i - 1
