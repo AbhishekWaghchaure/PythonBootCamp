89f66aaa84ab7e71152d6a256954a3fdc83cd81f
AbhishekWaghchaure <abhisw28@gmail.com>
2022-09-11 10:53:06 +0530

    added dictionary

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+marks = {"english": 75, "chemistry": 98}
+
+print(marks)
+print(marks["chemistry"])
+marks["physics"] = 97; #adding new key value in dictionary
+print(marks)
+
+marks["physics"] = 99
+print(marks)
