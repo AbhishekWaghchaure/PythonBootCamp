ae31ddedb8b55f48c57a69050b2d1fc0f721614b
AbhishekWaghchaure <abhisw28@gmail.com>
2022-09-11 10:27:33 +0530

    added tuple

---
+++ Tuple.py
@@ -0,0 +1,5 @@
+marks = [20, 30, 40, 50] #this is list mutable
+markss=(20, 30, 30, 30, 40, 50) #this is tuple immutable list
+
+print(markss.count(30))
+print(markss.index(30)) #index for first occurance of 30 will be printed
