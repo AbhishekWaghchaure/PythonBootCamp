4b3785242dd3c6a58394410ba2a8f4643ec8e307
AbhishekWaghchaure <abhisw28@gmail.com>
2022-09-13 20:03:50 +0530

    classes and inheritance

--- Classes.py
+++ Classes.py
@@ -23,3 +23,35 @@ class Person():
 p1 = Person("Slim shady", 23)
 p1.function()
 print(p1.age)
+
+class Person:
+    def __init__(self, firstName, lastName):
+        self.firstName = firstName
+        self.lastName = lastName
+
+    def printName(self):
+        print(self.firstName, self.lastName)
+
+x = Person("Abhishek", "Waghchaure")
+x.printName()
+
+# class Student(Person):
+#     pass
+#     def printRandom(self):
+#         print("in random method of child class")
+#
+# s = Student("xyz","abc")
+# s.printName()
+# s.printRandom()
+
+class Student(Person):
+    def __init__(self, firstName, lastName, domain, year):
+        Person.__init__(self, firstName, lastName)
+        self.domain = domain
+        self.year = year
+
+    def print(self):
+        print(self.firstName,self.lastName, self.domain, self.year)
+
+s = Student("Ram", "krishna", "DSA", "2022")
+s.print()
