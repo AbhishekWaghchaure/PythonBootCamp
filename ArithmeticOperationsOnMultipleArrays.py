53a0744d51edbcf18f8c12fe630d02027d2118f0
AbhishekWaghchaure <abhisw28@gmail.com>
2022-09-20 19:24:52 +0530

    operations on multiple arrays

---
+++ ArithmeticOperationsOnMultipleArrays.py
@@ -0,0 +1,14 @@
+import numpy as np
+arr = np.array([[1,2,3],
+                [4,5,6],
+                [7,8,9,]])
+arr2 = np.array([[9,8,7],
+                 [6,5,4],
+                 [3,2,1]])
+arr3 = np.array([[]])
+
+print(arr)
+print(arr2)
+
+print(arr+arr2)
+print(arr*arr2)
